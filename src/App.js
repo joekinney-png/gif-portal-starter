6352b7d7ab42a6e6a297e926080bccf6ad2b76c7
Joe Kinney <joe@enterpagoda.com>
2021-12-27 15:42:19 -0500

    add phantom wallet check

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "josephrkinney";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  /*
+   * This function holds the logic for determining whether or not a Solana wallet is found
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana && solana.isPhantom) {
+        console.log("Phantom wallet found");
+      } else {
+        alert("Solana object with Phantom not found! Get a Phantom Wallet 👻");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /*
+   * When the component first mounts, check to determine whether or not you have a connected Phantom wallet
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
